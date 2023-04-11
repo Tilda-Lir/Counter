350ce01bf581a3aa11ab81a1642ea47557c239cc
Tilda-Lir <morgandseli@gmail.com>
2023-04-11 10:28:50 +0400

    Initial Commit

---
+++ Counter/CounterApp.swift
@@ -0,0 +1,17 @@
+//
+//  CounterApp.swift
+//  Counter
+//
+//  Created by Сергей Скибинский on 11.04.2023.
+//
+
+import SwiftUI
+
+@main
+struct CounterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
